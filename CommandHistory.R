441bd75e8db2640399c5c197934550f60c36c659
Joseph Slone <joe.slone@gmail.com>
2015-10-22 21:55:41 -0400

    Finished w the data cleaning and analysis
    Just need to write it up.

--- CommandHistory.R
+++ CommandHistory.R
@@ -12,8 +12,7 @@ library(ggplot2)
 # Some colors and fonts
 
 bold.16.text <- element_text(face = "bold", size = 12)
-mypal <- colorRampPalette(brewer.pal(10, "Dark2"))
-
+mypal <- colorRampPalette(brewer.pal(8, "Dark2"))
 
 # Download and un-bzip the data file
 
@@ -40,77 +39,7 @@ fixCaps <- function(x) {
 new.names <- sapply(old.names, fixCaps)
 setnames(StormData, old.names, new.names)
 
-# Probably Not Necessary
 
-# StormData %>%
-#     mutate(Bgn.Date = as.Date(Bgn.Date, "%m/%d/%Y")) %>%
-#     mutate(Bgn.Time = as.character(Bgn.Time)) -> StormData
-
-
-# Fatality and Injuries by Event Type
-
-StormData %>% select(Evtype, Fatalities, Injuries) -> Mortality
-
-StormData %>%
-    select(Evtype, Fatalities, Injuries) %>%
-    group_by(Evtype) %>%
-    summarise_each(funs(sum)) -> Mortality.smry
-
-
-# Sorted by Fatilites
-Mortality.smry[order(-Fatalities)] %>%
-    select(Evtype, Fatalities) %>%
-    head(11) -> fatalities
-
-# Sorted by Injuries
-Mortality.smry[order(-Injuries)] %>%
-    select(Evtype, Injuries) %>%
-    head(11) -> injuries
-
-
-# Fatalities Plot
-ggplot(fatalities, aes(x = Evtype, y = Fatalities, fill=Evtype)) +
-    geom_bar(stat = "identity") +
-    theme(axis.text.x=element_text(angle=45, hjust=1)) +
-    theme(axis.text.x = bold.16.text) +
-    xlab("Event Type") + ylab("Fatalities") +
-    ggtitle("Top 10 Fatalities") +
-    scale_fill_manual(values = mypal(11))
-
-# Injuries Plot
-ggplot(injuries, aes(x = Evtype, y = Injuries, fill=Evtype)) +
-    geom_bar(stat = "identity") +
-    theme(axis.text.x=element_text(angle=45, hjust=1)) +
-    theme(axis.text.x = bold.16.text) +
-    xlab("Event Type") + ylab("Injuries") +
-    ggtitle("Top 10 Injuries") +
-    scale_fill_manual(values = mypal(11))
-
-# Economic Impact
-
-StormData %>%
-    select(Evtype, Propdmg, Cropdmg) %>%
-    group_by(Evtype) %>%
-    summarise_each(funs(sum)) -> Economics.smry
-
-property_dmg <- sum(Economics.smry$Propdmg)
-crop_dmg <- sum(Economics.smry$Cropdmg)
-
-property_top10 <- sum(head(sort(Economics.smry$Propdmg, decreasing = TRUE), 10))
-crop_top10 <- sum(head(sort(Economics.smry$Cropdmg, decreasing = TRUE), 10))
-
-# The top 10 event types over the years account for over 90% of the
-# property and crop damage reported.  Therefore, I think that simply reporting the
-# top 10 events of all classes will be enough.
-#
-# Except that Thunderstorm Wind and TSTM Wind should be lumped together.
-#
-# Arggh
-
-property_top10/property_dmg
-# 91%
-crop_top10/crop_dmg
-# 93%
 
 # From the Storm Event Table on pg 6 NWSI 10-1605 AUGUST 17, 2007
 
@@ -140,8 +69,10 @@ weather_event_labels <- toupper(c("Astronomical Low Tide",  "Avalanche",
                           "Winter Storm", "Winter Weather"))
 
 
+# All of the different Hurricanes
+
 StormData %>% filter(grepl("Hurricane|HURRICANE",Evtype)) -> HurricaneData
-Hurricanes <- as.character(HurricaneData$Evtype)
+Hurricanes <- unique(as.character(HurricaneData$Evtype))
 
 StormData %>%
     mutate(Evtype = as.character(Evtype)) %>%
@@ -154,19 +85,29 @@ StormData %>%
     mutate(EvClass = ifelse(Evtype == "FLASH FLOODING", "FLASH FLOOD", EvClass)) %>%
     mutate(EvClass = ifelse(Evtype == "URBAN/SML STREAM FLD", "FLOOD", EvClass)) %>%
     mutate(EvClass = ifelse(EvClass == "OTHER", paste("OTHER-", Evtype), EvClass)) %>%
-    select(EvClass, Propdmg, Cropdmg) %>%
+    select(EvClass, Propdmg, Cropdmg, Fatalities, Injuries) -> StormData.Cleaned
+
+StormData.Cleaned %>%
+    select(EvClass, Propdmg) %>%
     group_by(EvClass) %>%
-    summarise_each(funs(sum)) -> Economics.smry
+    summarise_each(funs(sum)) %>%
+    arrange(desc(Propdmg)) -> Property.Summary
 
-# This showed a typo "THUNDERSTORM WINDS" should be "THUNDERSTORM WIND"
-
-StormData %>%
-    mutate(Evtype = as.character(Evtype)) %>%
-    mutate(EvClass = ifelse(Evtype %in% weather_event_labels, Evtype, "OTHER")) %>%
-    mutate(EvClass = ifelse(Evtype %in% Hurricanes, "HURRICANE", EvClass )) %>%
-    mutate(EvClass = ifelse(Evtype == "THUNDERSTORM WINDS", "THUNDERSTORM WIND", EvClass)) %>%
-    select(EvClass, Propdmg, Cropdmg) %>%
+StormData.Cleaned %>%
+    select(EvClass, Cropdmg) %>%
     group_by(EvClass) %>%
-    summarise_each(funs(sum)) -> Economics.smry
+    summarise_each(funs(sum)) %>%
+    arrange(desc(Cropdmg)) -> Crops.Summary
 
-# OTHER- TSTM WIND appears to be a problem.
+StormData.Cleaned %>%
+    select(EvClass, Fatalities) %>%
+    group_by(EvClass) %>%
+    summarise_each(funs(sum)) %>%
+    arrange(desc(Fatalities)) -> Fatalities.Summary
+
+StormData.Cleaned %>%
+    select(EvClass, Injuries) %>%
+    group_by(EvClass) %>%
+    summarise_each(funs(sum)) %>%
+    arrange(desc(Injuries)) -> Injuries.Summary
+
